fc28e48c485868e689b1f932b9fc045e3ea552f4
Steven Jake ASOY <130660922+stevenjakeasoy@users.noreply.github.com>
2023-04-30 09:43:43 +0800

    Update drawer.dart

--- drawer/drawer.dart
+++ drawer/drawer.dart
@@ -1,12 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hnu_mis_announcement/drawer/drawerItem.dart';
 import 'package:hnu_mis_announcement/drawer/myinfomation.dart';
 import 'package:hnu_mis_announcement/drawer/updateAddress.dart';
 import 'package:hnu_mis_announcement/drawer/updateContactNum.dart';
+import 'package:hnu_mis_announcement/services/auth/auth_service.dart';
+import 'package:hnu_mis_announcement/utilities/dialogs/logout_dialog.dart';
+import 'package:hnu_mis_announcement/views/constants/route.dart';
 
-class MyDrawer extends StatelessWidget {
+class MyDrawer extends StatefulWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
+  @override
+  State<MyDrawer> createState() => _MyDrawerState();
+}
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+User? _user = _auth.currentUser;
+
+class DrawerHeaderWidget extends StatelessWidget {
+  final User? user;
+
+  const DrawerHeaderWidget({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DrawerHeader(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          const SizedBox(height: 8),
+          Text(
+            user?.displayName ?? 'Name',
+            style: const TextStyle(
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user?.email ?? '',
+            style: const TextStyle(
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _MyDrawerState extends State<MyDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,34 +61,64 @@ class MyDrawer extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(10.0, 60, 10, 0),
           child: Column(
             children: [
-              headerWidget(),
-              const SizedBox(height: 30,),
-              const Divider(thickness: 1, height: 15, color: Colors.grey,),
-              const SizedBox(height: 15,),
-              DrawerItems(
-                  name: 'My Information',
-                  icon: Icons.article_outlined,
-                onPressed: ()=> onItemPressed(context, index: 0),
+              DrawerHeaderWidget(user: _user),
+              const SizedBox(
+                height: 30,
               ),
-              const SizedBox(height: 10,),
+              const Divider(
+                thickness: 1,
+                height: 15,
+                color: Colors.grey,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              DrawerItems(
+                name: 'My Information',
+                icon: Icons.article_outlined,
+                onPressed: () => onItemPressed(context, index: 0),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
               DrawerItems(
                 name: 'Update Address',
                 icon: Icons.location_city,
-                onPressed: ()=> onItemPressed(context, index: 1),
+                onPressed: () => onItemPressed(context, index: 1),
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               DrawerItems(
                 name: 'Update Contact Number',
                 icon: Icons.contact_phone_outlined,
-                onPressed: ()=> onItemPressed(context, index: 2),
+                onPressed: () => onItemPressed(context, index: 2),
               ),
-              const SizedBox(height: 20,),
-              const Divider(thickness: 1, height: 15, color: Colors.grey,),
-              const SizedBox(height: 15,),
+              const SizedBox(
+                height: 20,
+              ),
+              const Divider(
+                thickness: 1,
+                height: 15,
+                color: Colors.grey,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
               DrawerItems(
                 name: 'Log Out',
                 icon: Icons.logout,
-                onPressed: ()=> onItemPressed(context, index: 3),
+                onPressed: () async {
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await AuthService.firebase().logout();
+                    if (!mounted) return;
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      loginRoute,
+                          (_) => false,
+                    );
+                  }
+                },
               ),
             ],
           ),
@@ -51,44 +126,24 @@ class MyDrawer extends StatelessWidget {
       ),
     );
   }
-  void onItemPressed(BuildContext context, {required int index}){
+
+  void onItemPressed(BuildContext context, {required int index}) {
     Navigator.pop(context);
 
-    switch(index){
+    switch (index) {
       case 0:
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const My_Info()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const My_Info()));
         break;
       case 1:
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const UpdateAddress()));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const UpdateAddress()));
         break;
       case 2:
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const UpdateContactNum()));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const UpdateContactNum()));
         break;
     }
   }
-  Widget headerWidget(){
-    const img = 'assets/Kimberly Amor.jpg';
-    return Column(
 
-      children:  [
-        const CircleAvatar(
-          radius: 40,
-            backgroundImage: AssetImage(img),
-        ),
-        const SizedBox(width: 20,),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(height: 10,),
-
-            Text('Kimberly Amor', style: TextStyle(fontSize: 16, color: Colors.black),),
-            SizedBox(height: 10,),
-            Text('amor.kimberly@hnu.edu.ph', style: TextStyle(fontSize: 16, color: Colors.black),),
-            SizedBox(height: 10,),
-            Text('Roman Catholic', style: TextStyle(fontSize: 16, color: Colors.black),)
-          ],
-        )
-      ],
-    );
-  }
 }
